f0db6bfb16ca11652a49a61a011acc72f9dc98ba
Dan Gilkerson <contact@dangilkerson.com>
2016-05-03 14:24:37 -0700

    Use glance extensions to highlight

--- src/demo.js
+++ src/demo.js
@@ -4,28 +4,53 @@ import randomColor from 'randomcolor';
 
 window.glanceSelector = glanceSelector;
 
-$(function(){
+var activeHighlighted = $();
+
+function clearHighlighted() {
+    activeHighlighted.each(function() {
+        $(this).css("background-color", $(this).data("original-background-color"))
+    });
+}
+
+function highlightElements(elements) {
+    clearHighlighted();
+
+    activeHighlighted = $(elements)
+
+    activeHighlighted.each(function() {
+        $(this).data("original-background-color", $(this).css('backgroundColor'))
+    }).css("background-color", randomColor({
+        luminosity: "light"
+    }))
+}
+
+glanceSelector.addExtension({
+    beforeAll: function(selector){
+
+        (function setDemoText(text) {
+            if($("#glance-selector").length == 0) {
+                setTimeout(function() { setDemoText(text) }, 250);
+            }
+            else {
+                $("#glance-selector").val(text);
+            }
+        })(selector);
+    },
+    afterFilter: function(target, elements) {
+        highlightElements(elements);
+        return elements;
+    }
+});
+
+$(function() {
     $("<div><input id='glance-selector'/></div>")
         .prependTo("body")
         .css({})
         .keyup(function() {
             var selector = $("#glance-selector").val();
-            highlight(selector)
+            if (selector == "")
+                clearHighlighted();
+            else
+                glanceSelector(selector)
         });
-
-    var activeHighlighted = $();
-
-    function highlight(selector) {
-        activeHighlighted.each(function() {
-            $(this).css("background-color", $(this).data("original-background-color"))
-        });
-
-        activeHighlighted = $(glanceSelector(selector))
-
-        activeHighlighted.each(function() {
-            $(this).data("original-background-color", $(this).css('backgroundColor'))
-        }).css("background-color", randomColor({
-            luminosity: "light"
-        }))
-    }
-})
+});
