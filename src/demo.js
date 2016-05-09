f6fa99bc2fe5eee55d1850eddbebbf3fa4a9828c
Dan Gilkerson <contact@dangilkerson.com>
2016-05-08 17:11:21 -0700

    Background color for toolbar

--- src/demo.js
+++ src/demo.js
@@ -18,8 +18,8 @@ function isDescendant(parent, child) {
 
 function clearHighlighted() {
     $('*[data-original-background-color]').each(function () {
-            $(this).css("background-color", $(this).data("original-background-color"))
-            $(this).removeAttr("data-original-background-color")
+        $(this).css("background-color", $(this).data("original-background-color"))
+        $(this).removeAttr("data-original-background-color")
     });
 }
 
@@ -73,27 +73,47 @@ $(function () {
         });
 
     var background = $("<div></div>")
-        .prependTo(toolbar)
+        .appendTo(toolbar)
         .css({
             "position": "fixed",
-            "height": "35px",
+            "height": "23px",
             "top": 0,
             "left": 0,
-            "right": 0
+            "right": 0,
+            "background-color": "#3D5980",
+            "padding": "4px"
+        });
+
+    var label = $("<label>Glance:&nbsp;</label>")
+        .appendTo(background)
+        .css({
+            "color": "white",
+            "float": "left",
+            "font-size": "20px",
+            "line-height": "24px"
+        });
+
+    var wrapper = $("<span></span>")
+        .appendTo(background)
+        .css({
+            "display": "block",
+            "overflow": "hidden"
         });
 
     $("<input id='glance-selector'/>")
-        .prependTo(background)
+        .appendTo(wrapper)
         .css({
-            "width": "100%",
             "font-size": "20px",
-            "padding": "4px"
+            "padding": "0px 0px 0px 4px",
+            "width": "100%",
+            "outline-width": 0,
+            "border": 0
         })
         .keyup(function () {
             var selector = $("#glance-selector").val();
             if (selector == "")
                 clearHighlighted();
             else
-                glanceSelector(selector)
+                glanceSelector(selector);
         });
 });
